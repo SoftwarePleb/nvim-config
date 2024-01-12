7adf46e413164ea5fce33af95f78c8c4c7c4ffae
softwarepleb <softwarepleb99@gmail.com>
2024-01-13 10:04:27 +1300

    cleanned up the config for plugins. Moved each plugin to its own file.

--- init.lua
+++ init.lua
@@ -17,29 +17,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-require("catppuccin").setup()
-vim.cmd.colorscheme("catppuccin")
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"lua", "javascript"},
-	highlight = {enable = true},
-	indent = {enable = true},
-})
