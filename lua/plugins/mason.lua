dc1c1e5e1be89840cfc6904e7af873922ca5d831
softwarepleb <softwarepleb99@gmail.com>
2024-01-14 22:21:05 +1300

    added stylelua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
       vim.keymap.set('n', 'gd', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.definition,  {})
       vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
