5776c4a17917f8f01bf4130d42bf9bc940c94654
softwarepleb <softwarepleb99@gmail.com>
2024-01-15 23:17:28 +1300

    added code to make go lsp work.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "csharp_ls" }
+        ensure_installed = { "lua_ls", "csharp_ls", "gopls" }
       })
     end
   },
@@ -19,6 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.csharp_ls.setup({})
       lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
       lspconfig.tsserver.setup({})
       vim.keymap.set('n', 'gd', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.definition, {})
