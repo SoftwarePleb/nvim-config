713caf8a641a3b9872342ceb24bd4540570144f4
softwarepleb <softwarepleb99@gmail.com>
2024-01-17 00:17:03 +1300

    added autosnip and auto complete

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,13 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.prettier
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
